310bd0ed6a7ce75d131efde6d6028e0c5a9461e2
Ben Degeneve <poompoomcha@gmail.com>
2021-11-26 15:44:33 +0100

    Login user

--- back/controllers/user.js
+++ back/controllers/user.js
@@ -1,5 +1,6 @@
 // Import des packages :
 const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
 
 const User = require('../models/User');
 
@@ -16,6 +17,31 @@ exports.signup = (req, res, next) => {
         .save()
         .then(() => res.status(201).json({ message: 'Utilisateur créé !' }))
         .catch((error) => res.status(400).json({ error }));
+    })
+    .catch((error) => res.status(500).json({ error }));
+};
+
+// Connexion d'un utilisateur existant :
+exports.login = (req, res, next) => {
+  User.findOne({ email: req.body.email })
+    .then((user) => {
+      if (!user) {
+        return res.status(401).json({ error });
+      }
+      bcrypt
+        .compare(req.body.password, user.password)
+        .then((valid) => {
+          if (!valid) {
+            return res.status(401).json({ error });
+          }
+          res.status(200).json({
+            userId: user._id,
+            token: jwt.sign({ userId: user._id }, 'TOKEN_SECRET_PHRASE', {
+              expiresIn: '24h',
+            }),
+          });
+        })
+        .catch((error) => res.status(500).json({ error }));
     })
     .catch((error) => res.status(500).json({ error }));
 };
